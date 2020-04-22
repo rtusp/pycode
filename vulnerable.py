24c9cac683c8c08cbcf1ce05ca14f80a6f84a34f
stream <stream@macbook-pro-mb.home>
2020-04-22 06:10:37 +0200

    Initial commit.

---
+++ vulnerable.py
@@ -0,0 +1,14 @@
+from flask import request, text
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from database.users import User
+
+@app.route('hello')
+def hello():
+    id = request.args.get("id")
+    stmt = text("SELECT * FROM users where id=%s" % id) # Query is constructed based on user inputs
+    query = SQLAlchemy().session.query(id) # Noncompliant
+    user = query.one()
+    return "Hello %s" % user.username
+
+
