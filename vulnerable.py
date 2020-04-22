409f6a0d6b1ed03f95098212a1a299345cb81110
stream <stream@macbook-pro-mb.home>
2020-04-22 11:05:23 +0200

    Make it vulnerable.

--- vulnerable.py
+++ vulnerable.py
@@ -6,7 +6,8 @@ from database.users import User
 @app.route('hello')
 def hello():
     id = request.args.get("id")
-    query = SQLAlchemy().session.query("SELECT 1") # Noncompliant
+    stmt = text("SELECT * FROM users where id=%s" % id) # Query is constructed based on user inputs
+    query = SQLAlchemy().session.query(id) # Noncompliant
     user = query.one()
     return "Hello %s" % user.username
 
