90fb0f8d3d2d7c2fdf953cf772e6a17f74e7978c
stream <stream@macbook-pro-mb.home>
2020-05-14 12:07:08 +0200

    Make code insecure

--- vulnerable.py
+++ vulnerable.py
@@ -2,6 +2,5 @@ from hashlib import pbkdf2_hmac
 import os
 
 password = raw_input()
-salt = os.urandom(8)
-hash = pbkdf2_hmac('sha1', password, salt, 1024)
+hash = pbkdf2_hmac('sha1', password, 'static salt', 1024)
 
